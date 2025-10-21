6effe7095db0e72993e2062c8076a998a595388c
Gabriel Cardoso <gabrielcardososouza3007@gmail.com>
2025-10-21 16:58:23 +0200

    Comparing the guessed number with the secret value, adding looping

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,26 +1,36 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("Good luck, you have the chance to guess the secret number!");
-
-    let mut guess = String::new();
-
-    // It's needed to add the "import", first line!
-    io::stdin() 
-    .read_line(&mut guess)
-    .expect("Failed to read line.");
     
-    let value = guess.trim().parse::<i32>().expect("Please type a number!");
-    let correct = value == secret_number;
+    loop {
+        println!("Good luck, you have the chance to guess the secret number!");
 
-    if correct {
-        println!("You hit");
-    } else {
-        println!("You loss, the secret number is {secret_number}");
+        let mut guess = String::new();
+
+
+        // It's needed to add the "import", first line!
+        io::stdin() 
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Comparing the secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            } 
+        }
     }
 }
